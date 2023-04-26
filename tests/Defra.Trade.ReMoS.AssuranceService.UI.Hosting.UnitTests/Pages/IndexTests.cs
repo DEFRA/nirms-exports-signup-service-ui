2fbc0b010f31995fce26442fbf85a97f67b982c5
Adrian Clare <adrian.clare@capgemini.com>
2023-04-26 17:14:52 +0100

    renamed projects and solution

--- tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/Pages/IndexTests.cs
+++ tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/Pages/IndexTests.cs
@@ -2,11 +2,6 @@
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Defra.ReMoS.AssuranceService.UI.UnitTests.Pages;
 
