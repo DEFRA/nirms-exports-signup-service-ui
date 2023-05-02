1c61ba7574a9db32e73f863159f5d8ce1e5cee9f
ryan corcoran <ryan.corcoran@defra.onmicrosoft.com>
2023-05-02 12:22:51 +0100

    Testing sonar with new file formats

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessCountry.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessCountry.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.RegisteredBusiness
 {
+    [ExcludeFromCodeCoverage]
     public class RegisteredBusinessCountryModel : PageModel
     {
         public void OnGet()
