c018743cb19303d88040f631f13dbc28fcdb7e4c
ryan corcoran <ryan.corcoran@defra.onmicrosoft.com>
2023-05-02 12:28:27 +0100

    Making method not empty

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessCountry.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessCountry.cshtml.cs
@@ -7,8 +7,11 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.Regis
     [ExcludeFromCodeCoverage]
     public class RegisteredBusinessCountryModel : PageModel
     {
+        [BindProperty]
+        public string? Country { get; set; }
         public void OnGet()
         {
+            Country = "United Kingdom";
         }
     }
 }
