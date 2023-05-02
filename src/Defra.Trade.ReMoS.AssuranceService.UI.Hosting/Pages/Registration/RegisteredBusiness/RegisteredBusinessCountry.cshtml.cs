09c45952524ff2ea9c80794a58df8e0511de17e0
ryan corcoran <ryan.corcoran@defra.onmicrosoft.com>
2023-05-02 12:02:59 +0100

    Gov UK implemented with compiled css, header and footer added and start of page navigation structure implemented

---
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessCountry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.RegisteredBusiness
+{
+    public class RegisteredBusinessCountryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
