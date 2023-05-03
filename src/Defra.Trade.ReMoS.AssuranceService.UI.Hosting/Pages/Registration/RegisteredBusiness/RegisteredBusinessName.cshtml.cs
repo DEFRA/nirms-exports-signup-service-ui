8faf00f06dde4dd002b3bbe8e610c633bc4fd976
Adrian Clare <adrian.clare@capgemini.com>
2023-05-03 14:59:50 +0100

    validation & tests

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessName.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/RegisteredBusinessName.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.RegisteredBusiness
@@ -8,9 +9,34 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.Regis
     public class RegisteredBusinessNameModel : PageModel
     {
         [BindProperty]
-        public string? Name { get; set; }
-        public void OnGet()
+        [Required(ErrorMessage = "Enter your business name")]
+        [RegularExpression(@"^[a-zA-Z0-9'-]*$", ErrorMessage = "Enter your business name using only letters, numbers, hyphens (-) and apostrophes (')")]
+        [MaxLength(100, ErrorMessage = "Business name is too long")]
+        public string Name { get; set; } = string.Empty;
+
+        private readonly ILogger<RegisteredBusinessNameModel> _logger;
+
+        public RegisteredBusinessNameModel(ILogger<RegisteredBusinessNameModel> logger)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            _logger.LogInformation("Business Name OnGet");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostSubmitAsync()
+        {
+            _logger.LogInformation("Business Name OnPostSubmit");
+
+            if (!ModelState.IsValid)
+            {
+                return await OnGetAsync();
+            }
+
+            return await OnGetAsync();
         }
     }
 }
---
+++ tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/Registration/RegisteredBusinessNameTests.cs
@@ -0,0 +1,45 @@
+﻿using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.RegisteredBusiness;
+using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Shared;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Registration
+{
+    public class RegisteredBusinessNameTests : PageModelTestsBase
+    {
+        private RegisteredBusinessNameModel _systemUnderTest;
+        protected Mock<ILogger<RegisteredBusinessNameModel>> _mockLogger = new();
+
+        public RegisteredBusinessNameTests()
+        {
+            _systemUnderTest = new RegisteredBusinessNameModel(_mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task OnGet_NoNamePresentIfNoSavedData()
+        {
+            //Arrange
+            //TODO: Add setup for returning values when API referenced
+
+            //Act
+            await _systemUnderTest.OnGetAsync();
+
+            //Assert
+            _systemUnderTest.Name.Should().Be("");
+        }
+
+        [Fact]
+        public async Task OnPostSubmit_SubmitValidInformation()
+        {
+            //Arrange
+            _systemUnderTest.Name = "";
+
+            //Act
+            await _systemUnderTest.OnPostSubmitAsync();
+            var validation = ValidateModel(_systemUnderTest);
+
+            //Assert
+            validation.Count.Should().Be(1);
+        }
+    }
+}
