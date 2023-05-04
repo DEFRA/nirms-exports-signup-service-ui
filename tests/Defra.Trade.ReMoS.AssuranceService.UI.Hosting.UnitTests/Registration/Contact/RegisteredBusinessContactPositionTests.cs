8ecd7fa505827209a1f852d75d2c34f9ce00122a
padget.avery <padget.avery@capgemini.com>
2023-05-04 11:17:26 +0100

    add position

---
+++ tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/Registration/Contact/RegisteredBusinessContactPositionTests.cs
@@ -0,0 +1,78 @@
+﻿using Castle.Core.Logging;
+using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Shared;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using FluentAssertions;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Registration;
+
+public class RegisteredBusinessContactPositionTests : PageModelTestsBase
+{
+    private RegisteredBusinessContactPositionModel _systemUnderTest;
+    protected Mock<ILogger<RegisteredBusinessContactPositionModel>> _mockLogger = new();
+
+    public RegisteredBusinessContactPositionTests()
+    {
+        _systemUnderTest = new RegisteredBusinessContactPositionModel(_mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task OnGet_NoPositionPresentIfNoSavedData()
+    {
+        // arrange
+        // TODO add setup for returning values when api referenced
+
+        // act
+        await _systemUnderTest.OnGetAsync();
+
+        // assert
+        _systemUnderTest.Position.Should().Be("");
+    }
+
+    [Fact]
+    public async Task OnPostSubmit_SubmitValidInformation()
+    {
+        // arrange
+        _systemUnderTest.Position = "aZ9 -'";
+
+        // act
+        await _systemUnderTest.OnPostSubmitAsync();
+        var validation = ValidateModel(_systemUnderTest);
+
+        // assert
+        validation.Count.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task OnPostSubmit_SubmitInvalidCharacterInformation()
+    {
+        // arrange
+        _systemUnderTest.Position = "*";
+
+        // act
+        await _systemUnderTest.OnPostSubmitAsync();
+        var validation = ValidateModel(_systemUnderTest);
+
+        // assert
+        validation.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task OnPostSubmit_SubmitTooManyCharactersInformation()
+    {
+        // arrange
+        _systemUnderTest.Position = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        // act
+        await _systemUnderTest.OnPostSubmitAsync();
+        var validation = ValidateModel(_systemUnderTest);
+
+        // assert
+        validation.Count.Should().Be(1);
+    }
+}
