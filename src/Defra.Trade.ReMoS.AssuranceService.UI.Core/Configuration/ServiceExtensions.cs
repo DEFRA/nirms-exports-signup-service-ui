98d7c0e5b699bf9a551b89ae5ff31a410f9da7c8
ryan corcoran <ryan.corcoran@defra.onmicrosoft.com>
2023-06-16 08:57:27 +0100

    Removal of now un-needed logic

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Core/Configuration/ServiceExtensions.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Core/Configuration/ServiceExtensions.cs
@@ -15,12 +15,7 @@ public static class ServiceExtensions
     {
         services.AddHttpClient("Assurance", httpClient =>
         {
-            //DEV
             httpClient.BaseAddress = new Uri($"{config.GetValue<string>("APISettings:APIUrl")}/");
-
-            //Local
-            //httpClient.BaseAddress = new Uri($"{config.GetValue<string>("APISettings:APIUrl")}");
-
             httpClient.DefaultRequestHeaders.Add("x-api-version", "1");
         });
 
