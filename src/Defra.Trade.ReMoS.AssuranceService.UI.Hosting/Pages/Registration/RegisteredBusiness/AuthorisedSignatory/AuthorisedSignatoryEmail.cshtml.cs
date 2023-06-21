608f99366dbcff6595d5db2c87f8ddaae731392a
Adrian Clare <adrian.clare@capgemini.com>
2023-06-21 12:12:26 +0100

    Add tests to authorised signatory pages

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/AuthorisedSignatory/AuthorisedSignatoryEmail.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/RegisteredBusiness/AuthorisedSignatory/AuthorisedSignatoryEmail.cshtml.cs
@@ -12,6 +12,7 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.Regis
     {
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         [BindProperty]
+        [Required(ErrorMessage = "Enter a email.")]
         public string? Email { get; set; }
         [BindProperty]
         public string? BusinessName { get; set; }
@@ -34,7 +35,7 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.Regis
             TraderId = id;
             
             var party = await GetSignatoryEmailFromApiAsync();
-            BusinessName = party.PartyName;
+            BusinessName = party?.PartyName;
             return Page();
         }
 
---
+++ tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/Registration/AuthorisedSignatory/AuthorisedSignatoryNameTests.cs
@@ -0,0 +1,77 @@
+﻿using Defra.Trade.ReMoS.AssuranceService.UI.Core.DTOs;
+using Defra.Trade.ReMoS.AssuranceService.UI.Core.Interfaces;
+using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Registration.RegisteredBusiness.AuthorisedSignatory;
+using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Shared;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Registration.AuthorisedSignatory
+{
+    [TestFixture]
+    public class AuthorisedSignatoryNameTests : PageModelTestsBase
+    {
+        private AuthorisedSignatoryNameModel? _systemUnderTest;
+
+        protected Mock<ILogger<AuthorisedSignatoryNameModel>> _mockLogger = new();
+        protected Mock<ITraderService> _mockTraderService = new();
+
+
+        [SetUp]
+        public void TestCaseSetup()
+        {
+            _systemUnderTest = new AuthorisedSignatoryNameModel(_mockTraderService.Object, _mockLogger.Object);
+        }
+
+        [Test]
+        public async Task OnGet_PopulateModelProperties()
+        {
+            //Arrange
+            var tradePartyId = Guid.NewGuid();
+
+            _mockTraderService
+                .Setup(x => x.GetTradePartyByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new Core.DTOs.TradePartyDTO()
+                {
+                    Id = tradePartyId,
+                    AuthorisedSignatory = new Core.DTOs.AuthorisedSignatoryDTO()
+                    {
+                        Id = Guid.NewGuid(),
+                    }
+                });
+
+            //Act
+            await _systemUnderTest!.OnGetAsync(Guid.NewGuid());
+
+            //Assert
+            _systemUnderTest.TradePartyId.Should().NotBeEmpty();
+            _systemUnderTest.SignatoryId.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task OnPostSubmit_InvalidInput()
+        {
+            //Arrange
+            var tradePartyId = new Guid("50919f18-fb85-450a-81a9-a25e7cebc0ff");
+
+            _mockTraderService
+                .Setup(x => x.GetTradePartyByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new Core.DTOs.TradePartyDTO()
+                {
+                    AuthorisedSignatory = new AuthorisedSignatoryDTO { }
+                });
+
+            //Act
+            await _systemUnderTest!.OnPostSubmitAsync();
+
+            //Assert
+
+            var validation = ValidateModel(_systemUnderTest);
+            validation.Count.Should().Be(1);
+        }
+    }
+}
