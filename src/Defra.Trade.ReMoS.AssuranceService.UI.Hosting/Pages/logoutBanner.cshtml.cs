426595ccf80d9663e83e6afbaabea1afaf6c0f2d
padget.avery <padget.avery@capgemini.com>
2023-07-26 11:24:59 +0100

    allow anonymous on logout

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/logoutBanner.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/logoutBanner.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages
 {
+    [AllowAnonymous]
     public class logoutBannerModel : PageModel
     {
         public void OnGet()
