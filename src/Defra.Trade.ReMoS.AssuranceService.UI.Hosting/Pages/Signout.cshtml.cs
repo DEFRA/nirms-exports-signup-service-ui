801723b7d08d9b681e2c531d0f3274de2f6b0fbc
padget.avery <padget.avery@capgemini.com>
2023-07-26 11:11:12 +0100

    add ehco logout

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Signout.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Signout.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
+using Defra.Trade.ReMoS.AssuranceService.UI.Core.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages
 {
@@ -10,10 +12,12 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages
     public class SignoutModel : PageModel
     {
         private readonly ILogger _logger;
+        private readonly IOptions<EhcoIntegration> _ehcoIntegrationSettings;
 
-        public SignoutModel(ILogger<SignoutModel> logger)
+        public SignoutModel(ILogger<SignoutModel> logger, IOptions<EhcoIntegration> ehcoIntegrationSettings)
         {
             _logger = logger;
+            _ehcoIntegrationSettings = ehcoIntegrationSettings;
         }
 
         public async Task<IActionResult> OnGet()
@@ -21,9 +25,13 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages
             _logger.LogInformation("User {Name} logged out at {Time}.",
                 User.Identity!.Name, DateTime.UtcNow);
 
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);         
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme); 
+            
+            var correlationId = Guid.NewGuid();
 
-            Response.Redirect("https://www.gov.uk/export-health-certificates/general-certificate-for-moving-goods-under-the-ni-retail-movement-scheme");
+            Response.Redirect(_ehcoIntegrationSettings.Value.ValidIssuer + "/b2c/remos_signup/correlationId/" + correlationId + "/logout");
+
+            //Response.Redirect("https://www.gov.uk/export-health-certificates/general-certificate-for-moving-goods-under-the-ni-retail-movement-scheme");
 
             return Page();
         }
