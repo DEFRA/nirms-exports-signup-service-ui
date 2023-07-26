d0597385d6916603a933fd2e56398962cd5dba78
padget.avery <padget.avery@capgemini.com>
2023-07-26 11:24:25 +0100

    add logout banner page

---
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/logoutBanner.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages
+{
+    public class logoutBannerModel : PageModel
+    {
+        public void OnGet()
+        {
+            Response.Redirect("https://www.gov.uk/export-health-certificates/general-certificate-for-moving-goods-under-the-ni-retail-movement-scheme");
+        }
+    }
+}
