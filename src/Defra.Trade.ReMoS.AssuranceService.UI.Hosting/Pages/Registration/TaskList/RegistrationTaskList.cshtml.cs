ef71701929ce0a2ab4c02c3875839735439c60bd
ryan corcoran <ryan.corcoran@defra.onmicrosoft.com>
2023-08-21 16:15:10 +0100

    Wording updated for cannot register page and establishments changed to show how many added

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/TaskList/RegistrationTaskList.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/Registration/TaskList/RegistrationTaskList.cshtml.cs
@@ -29,6 +29,7 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.TaskList
         public string ReviewAnswers { get; set; } = TaskListStatus.CANNOTSTART;
         public string? Country { get; set; }
         public bool EstablishmentsAdded { get; set; }
+        public int EstablishmentsCount { get; set; }
         #endregion
 
         private readonly ILogger<RegistrationTaskListModel> _logger;
@@ -100,11 +101,13 @@ namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.TaskList
 
             if (Country != "NI" && establishments != null && establishments!.Any(x => x.NI_GBFlag == "GB"))
             {
+                EstablishmentsCount = establishments!.Count(x => x.NI_GBFlag == "GB");
                 EstablishmentsAdded = true;
             }
 
             if (Country == "NI" && establishments != null && establishments!.Any(x => x.NI_GBFlag == "NI"))
             {
+                EstablishmentsCount = establishments!.Count(x => x.NI_GBFlag == "NI");
                 EstablishmentsAdded = true;
             }
 
