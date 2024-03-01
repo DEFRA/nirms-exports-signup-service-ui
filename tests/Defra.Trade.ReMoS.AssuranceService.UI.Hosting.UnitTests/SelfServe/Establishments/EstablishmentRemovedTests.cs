6f1fced0c811c40c70b8c5115819283e43d13376
Muhammad Ali Shah <113431816+alidfe@users.noreply.github.com>
2024-03-01 13:12:12 +0000

    Feature/285227 remove establishment (#3)
    * Update approval status and modified date
    
    * Added tests

---
+++ tests/Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests/SelfServe/Establishments/EstablishmentRemovedTests.cs
@@ -0,0 +1,36 @@
+﻿using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.SelfServe.Establishments;
+using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.Shared;
+
+namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.UnitTests.SelfServe.Establishments;
+
+[TestFixture]
+public class EstablishmentRemovedTests : PageModelTestsBase
+{
+    private EstablishmentRemovedModel? _systemUnderTest;
+
+    [SetUp]
+    public void TestCaseSetup()
+    {
+        _systemUnderTest = new EstablishmentRemovedModel()
+        {
+            PageContext = PageModelMockingUtils.MockPageContext()
+        };
+    }
+
+    [TestCase("Dep warehouse", "GB")]
+    [TestCase("Dest warehouse", "NI")]
+    public void OnGet_ModelPropertiesSet_WhenValuesGiven(string locName, string NI_GB_Flag)
+    {
+        // arrange
+        var id = Guid.NewGuid();
+
+        // act
+        var result = _systemUnderTest?.OnGet(id, locName, NI_GB_Flag);
+
+        // assert
+        result.Should().NotBeNull();
+        _systemUnderTest?.OrgId.Should().Be(id);
+        _systemUnderTest?.EstablishmentName.Should().Be(locName);
+        _systemUnderTest?.NI_GBFlag.Should().Be(NI_GB_Flag);
+    }
+}
