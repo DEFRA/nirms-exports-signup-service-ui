ca8c1729a724267813213ec6549aa5c9cb966da4
padget.avery <padget.avery@capgemini.com>
2024-03-28 14:10:49 +0000

    remove results page duplications

--- src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/SelfServe/Establishments/PostcodeSearch.cshtml.cs
+++ src/Defra.Trade.ReMoS.AssuranceService.UI.Hosting/Pages/SelfServe/Establishments/PostcodeSearch.cshtml.cs
@@ -1,12 +1,12 @@
 using Defra.Trade.ReMoS.AssuranceService.UI.Core.Configuration;
 using Defra.Trade.ReMoS.AssuranceService.UI.Core.Interfaces;
-using Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Shared.Establishments;
+using SignUp = Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.Establishments;
 using Microsoft.FeatureManagement.Mvc;
 
 namespace Defra.Trade.ReMoS.AssuranceService.UI.Hosting.Pages.SelfServe;
 
 [FeatureGate(FeatureFlags.SelfServeMvpPlus)]
-public class PostcodeSearchModel : PostcodeSearchModelBase
+public class PostcodeSearchModel : SignUp.PostcodeSearchModel
 {
     public PostcodeSearchModel(ILogger<PostcodeSearchModel> logger, ITraderService traderService) : base(logger, traderService)
     { }
